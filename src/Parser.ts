c58fb62eaf16221a68cf78065d15111c99a9b32e
tanvirtin <tanvir.tinz@gmail.com>
2021-10-23 21:48:35 -0400

    rename things

--- src/Parser.ts
+++ src/Parser.ts
@@ -18,28 +18,39 @@ import TokenType from "./TokenType.ts";
 import Token from "./Token.ts";
 
 /*
-  WARNING: This is not Pratt Parsing, the meaning of precedence means something different here.
-           Precedence here is more like binding power.
+  WARNING: This is not any parsing algorithm we are aware of this is a custom algorithm I implemented.
+           Use with caution requires verbose testing beforehand.
   RULES:
-  - Precedence directly influences binding power of an expression. Operators with higher precedence will bind whatever expression
-    there exists currently as it's left expression. This means higher precedence increases it's binding power.
-  - When precedence incrases we get the item from completed stack and use it as left for our new operator we just encountered.
-    Increase in precedence -> New operator has more left binding power. 
-  - When precedence drops, we pop the last pending expression and pop the last completed expression.
-    Then the last pending expression right gets assigned to the last completed expression.
-    NOTE: This is only allowed when the current operator precedence is greater than the pending operator precedence.
-    Decrease in precedence -> New opreator has more right binding power. 
-  - If the precedence is unchanged the behaviour will be the same when precedence drops.
-  - Formula for calculating precedence for an operator is (precedence = operator defined precedence)**depth
+  - We keep a stack of binding powers indicating the history of bindings powers so far.
+  - Initially this stack starts with [1, 1].
+  - Encountering a particular operator might add a binding power to this stack.
+  - We will keep three primary stacks for expression computation:
+      - pendingExpressions (Contains incomplete expreessions waiting to be completed)
+      - pendingExpressionBindingPowers (Contains the corresponding binding powers for the pending expressions)
+      - completedExpressions (Contains all the completed expressions yet to be dealt with)
+    When binding power incrases we get the last item from completed stack and use it as left for our new operator we just encountered.
+    Increase in binding power -> New operator has more left binding power. 
+    NOTE: The assumption is that there will always be an item in the completed stack when this scenario occurs.
+    NOTE: Operators that out weigh the previous binding power must always have a left expression that it needs to bind to.
+  - When binding power drops, we pop the last pending expression and pop the last completed expression.
+    Then the last pending expression's right gets assigned to the last completed expression.
+    A pending expression will always have a right expression that needs to be completed.
+    After we complete the binding the expression is now completed, it then gets pushed to the completed stack,
+    and this logic repeats until there are no more expressions in the completed stack. Unless we have finished computing
+    all the tokens, this operation should ONLY repeat if the current binding power of the new operator is greater
+    then whatever pending expression we are dealing with.
+    Decrease in binding power -> We complete all pending expressions by binding the it's right arm to whatever is in the completed stack. 
+  - If the binding power is unchanged the behaviour will be the same when binding power drops.
+  - Formula for calculating binding power for an operator is (binding power = user defined value)**depth
   - Anything in inside parenthesis will bump up the depth variable. The depth variable drops when the code
-    leaves the right parenthesis. This is how we controll the right and left tug of war via precedence.
+    leaves the right parenthesis. This is how we controll the right and left tug of war via binding power.
 */
 
 export default class Parser {
   private tokens: Token[];
   private cursor: number = 0;
-  private precedenceHistory: number[] = [1, 1];
-  private pendingPrecedence: number[] = [];
+  private bindingPowerHistory: number[] = [1, 1];
+  private pendingExpressionBindingPowers: number[] = [];
   private pendingExpressions: Expression[] = [];
   private completedExpressions: Expression[] = [];
   private depth = 1;
@@ -98,53 +109,53 @@ export default class Parser {
   }
 
   private completePendingExpressions(
-    respect_precedence: boolean,
+    respect_bindingPower: boolean,
   ): Expression | undefined {
-    let lastPending;
+    let lastPendingExpression;
     while (this.hasCompletedExpressions()) {
-      lastPending = this.pendingExpressions.at(-1);
-      var lastPendingPrecedence = this.pendingPrecedence.at(-1);
-      var lastCompleted = this.completedExpressions.pop();
+      lastPendingExpression = this.pendingExpressions.at(-1);
+      var lastPendingBindingPower = this.pendingExpressionBindingPowers.at(-1);
+      var lastCompletedExpression = this.completedExpressions.pop();
       if (
-        respect_precedence && lastPendingPrecedence &&
-        this.currentPrecedence() > lastPendingPrecedence
+        respect_bindingPower && lastPendingBindingPower &&
+        this.currentBindingPower() > lastPendingBindingPower
       ) {
-        return lastCompleted;
+        return lastCompletedExpression;
       }
-      if (lastPending != null) {
+      if (lastPendingExpression != null) {
         this.pendingExpressions.pop();
-        this.pendingPrecedence.pop();
-        lastPending.right = lastCompleted;
-        this.completedExpressions.push(lastPending);
+        this.pendingExpressionBindingPowers.pop();
+        lastPendingExpression.right = lastCompletedExpression;
+        this.completedExpressions.push(lastPendingExpression);
       } else {
-        lastPending = lastCompleted;
+        lastPendingExpression = lastCompletedExpression;
       }
     }
-    return lastPending;
+    return lastPendingExpression;
   }
 
-  private setPrecedence(value: number) {
-    this.precedenceHistory.push(value ** this.depth);
+  private setBindingPower(value: number) {
+    this.bindingPowerHistory.push(value ** this.depth);
   }
 
-  private currentPrecedence() {
-    const currentPrecedence = this.precedenceHistory.at(-1);
-    if (currentPrecedence == null) {
-      throw new Error("Failed to retrieve current precedence");
+  private currentBindingPower() {
+    const currentBindingPower = this.bindingPowerHistory.at(-1);
+    if (currentBindingPower == null) {
+      throw new Error("Failed to retrieve current bindingPower");
     }
-    return currentPrecedence;
+    return currentBindingPower;
   }
 
-  private lastPrecedence() {
-    const lastPrecedence = this.precedenceHistory.at(-2);
-    if (lastPrecedence == null) {
-      throw new Error("Failed to retrieve last precedence");
+  private lastBindingPower() {
+    const lastBindingPower = this.bindingPowerHistory.at(-2);
+    if (lastBindingPower == null) {
+      throw new Error("Failed to retrieve last bindingPower");
     }
-    return lastPrecedence;
+    return lastBindingPower;
   }
 
-  private hasPrecedenceIncreased() {
-    return this.currentPrecedence() > this.lastPrecedence();
+  private hasBindingPowerIncreased() {
+    return this.currentBindingPower() > this.lastBindingPower();
   }
 
   private isUnaryExpression(): boolean {
@@ -189,13 +200,13 @@ export default class Parser {
   }
 
   private binaryExpression(operator: Token) {
-    if (this.hasPrecedenceIncreased()) {
+    if (this.hasBindingPowerIncreased()) {
       var expression = this.completedExpressions.pop();
     } else {
       expression = this.completePendingExpressions(true);
     }
     if (expression != null) {
-      this.pendingPrecedence.push(this.currentPrecedence());
+      this.pendingExpressionBindingPowers.push(this.currentBindingPower());
       this.pendingExpressions.push(
         new BinaryExpression(expression, operator, undefined),
       );
@@ -226,9 +237,7 @@ export default class Parser {
       // Assignments done on a variable is actually an expression. (var a = 3; print a = 99;) should output 99, since print = 99; produces a value.
       } else if (this.matchToken(TokenType.EQUAL)) {
         // NOTE: we just need to look for the equal sign.
-        // With the highest possible precedence, whenever precedence drops this operator will
-        // be pending and then bind everything to it's right as it's right expression.
-        this.setPrecedence(8);
+        this.setBindingPower(8);
         this.assignmentExpression(token);
       } else if (this.matchToken(TokenType.IDENTIFIER)) {
         this.variableExpression(token);
@@ -236,13 +245,13 @@ export default class Parser {
         this.isUnaryExpression() &&
         this.matchToken(TokenType.BANG, TokenType.MINUS)
       ) {
-        this.setPrecedence(7);
+        this.setBindingPower(7);
         this.unaryExpression(token);
       } else if (
         this.isBinaryExpression() &&
         this.matchToken(TokenType.BANG_EQUAL, TokenType.EQUAL_EQUAL)
       ) {
-        this.setPrecedence(3);
+        this.setBindingPower(3);
         this.binaryExpression(token);
       } else if (
         this.isBinaryExpression() &&
@@ -253,19 +262,19 @@ export default class Parser {
           TokenType.LESS_EQUAL,
         )
       ) {
-        this.setPrecedence(4);
+        this.setBindingPower(4);
         this.binaryExpression(token);
       } else if (
         this.isBinaryExpression() &&
         this.matchToken(TokenType.MINUS, TokenType.PLUS)
       ) {
-        this.setPrecedence(5);
+        this.setBindingPower(5);
         this.binaryExpression(token);
       } else if (
         this.isBinaryExpression() &&
         this.matchToken(TokenType.SLASH, TokenType.STAR)
       ) {
-        this.setPrecedence(6);
+        this.setBindingPower(6);
         this.binaryExpression(token);
       } else if (this.matchToken(TokenType.LEFT_PAREN)) {
         ++this.depth;
